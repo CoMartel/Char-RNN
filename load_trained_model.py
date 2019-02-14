9373c6a6f24e18872eaf75afa3ccfe52cba03585
Corentin Martel <corentin.martel@corelyo.com>
2019-02-14 12:34:41 +0100

    Added the outpout and the files from a model trained on pandas and django

--- load_trained_model.py
+++ load_trained_model.py
@@ -6,17 +6,18 @@ from keras.models import load_model
 import pickle
 
 # Parameters
-origin = "full_pandas"
-model_name = "full_pandas_usize512_maxlen240_numlayers3_dropout0"
+origin = "full_pandas_with_import"
+model_name = "full_pandas_with_import_usize512_maxlen240_numlayers3_dropout0"
+weights = "weights-13-0.94.h5"
 length = 10000
 keep_chars = 50
 
 
-model = load_model("saved_models/{}/final.h5".format(model_name))
-char_to_indices = pickle.load(open("saved_models/{}c2i.p".format(origin), "rb"))
-indices_to_char = pickle.load(open("saved_models/{}i2c.p".format(origin), "rb"))
+model = load_model("saved_models/{}/{}".format(model_name,weights))
+char_to_indices = pickle.load(open("saved_models/{}_c2i.p".format(origin), "rb"))
+indices_to_char = pickle.load(open("saved_models/{}_i2c.p".format(origin), "rb"))
 
-for temperature in [0.2, 0.35, 0.5,1,2,3]:
+for temperature in [0.1,0.35,0.5,1,2]:
     generated_string = test_model(model=model,
                                   char_to_indices=char_to_indices,
                                   indices_to_char=indices_to_char,
